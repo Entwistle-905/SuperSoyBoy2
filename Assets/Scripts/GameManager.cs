f46c03858b880e472f6b87c1a0d6e592d0c2390b
Entwistle-905 <whonineofive@gmail.com>
2023-12-08 10:32:28 -0500

    Chapter13 Adding a Simple Level Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
     void Start()
     {
-        
+
+    }
+
+    public void RestartLevel(float delay)
+    {
+        StartCoroutine(RestartLevelDelay(delay));
+    }
+
+    private IEnumerator RestartLevelDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Game");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
